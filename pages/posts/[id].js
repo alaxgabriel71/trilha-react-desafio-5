e916711a1a277d27765cbfe1153c35ea609b842f
alaxgabriel71 <alaxgabriel71@gmail.com>
2024-09-25 18:07:04 -0300

    desafio concluído

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -26,8 +26,8 @@ export default function PostPage({
   return (
     <Layout>
       <SEO
-        title={`${posts.title} - ${globalData.name}`}
-        description={posts.description}
+        title={`${posts?.title} - ${globalData.name}`}
+        description={posts?.description}
       />
       <Header name={globalData.name} />
       <article className="px-6 md:px-0">
@@ -41,7 +41,7 @@ export default function PostPage({
         </header>
         <main>
           <article className="prose dark:prose-dark">
-            {posts.body}
+            {posts?.body}
           </article>
         </main>
       </article>
@@ -60,8 +60,7 @@ export default function PostPage({
 
 export const getServerSideProps = async ({ params }) => {
   const globalData = getGlobalData();
-  const posts = await getPostBySlug(params.id);
- 
+  const posts = await getPostBySlug(params.id); 
 
   return {
     props: {
--- services/api.js
+++ services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://evxnwxeaprvobxovehby.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2eG53eGVhcHJ2b2J4b3ZlaGJ5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzI5MDYwOCwiZXhwIjoyMDQyODY2NjA4fQ.qLOEdKetmZbR8xYeuea9gC_y9ZSTiWItLdrpHN81sSc",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2eG53eGVhcHJ2b2J4b3ZlaGJ5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzI5MDYwOCwiZXhwIjoyMDQyODY2NjA4fQ.qLOEdKetmZbR8xYeuea9gC_y9ZSTiWItLdrpHN81sSc"
+        
     }
 })
